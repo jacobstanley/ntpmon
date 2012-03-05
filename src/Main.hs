3b1ea0710f10595eb2054e803652c2da43e10824
Jacob Stanley <jacob@stanley.io>
2012-03-06 00:54:43 +0800

    Support for monitoring servers relative to each other

--- src/Main.hs
+++ src/Main.hs
@@ -7,118 +7,181 @@ module Main where
 import Control.Applicative ((<$>))
 import Control.Concurrent (threadDelay)
 import Control.Exception (bracket)
-import Control.Monad (forever)
 import Data.List (intercalate)
 import Data.Serialize
+import Data.Time.Calendar (fromGregorian)
 import Data.Time.Clock
-import Data.Time.Format
+import Data.Word (Word64)
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString
 import System.CPUTime.Rdtsc (rdtsc)
-import System.Locale (defaultTimeLocale)
+import System.IO
+import System.Posix.Clock (Clock (..), TimeSpec (..), getTime)
+import System.Timeout (timeout)
 import Text.Printf (printf)
 
-import Data.NTP
+import Data.NTP hiding (Server, getTime)
 
 ------------------------------------------------------------------------
 
-servers :: [HostName]
-servers = --[ "localhost"
-          --, "0.au.pool.ntp.org"
-          --, "1.au.pool.ntp.org"
-          --, "2.au.pool.ntp.org"
-          --, "3.au.pool.ntp.org"
-          [ "203.82.209.217"
-          ]
-
-resolveAddrs :: [HostName] -> IO [(HostName, SockAddr)]
-resolveAddrs xs = concat . zipWith (zip . repeat) xs <$> mapM ntpAddrs xs
+hosts :: [HostName]
+hosts =
+    [ "localhost"
+    , "time.uwa.edu.au"
+    , "203.171.85.237" -- PPS
+    , "ntp.ii.net"
+    , "JakeAir.local"
+    --, "au.pool.ntp.org"
+    --, "1.au.pool.ntp.org"
+    --, "2.au.pool.ntp.org"
+    --, "3.au.pool.ntp.org"
+    --, "fi.pool.ntp.org"
+    ]
 
 main :: IO ()
 main = withSocketsDo $ do
-    --putStrLn "NTP Monitor"
-    addrs <- resolveAddrs servers
-
-    bracket ntpSocket sClose $ \sock -> do
-    forever $ do
-        mapM_ (roundtrip sock) addrs
-        threadDelay 1000000
+    hSetBuffering stdout LineBuffering
+    servers <- concat <$> mapM getServers hosts
+    putStrLn (intercalate "," (map showAddress servers))
+    bracket udpSocket sClose (loop servers)
   where
-    roundtrip :: Socket -> (HostName, SockAddr) -> IO ()
-    roundtrip sock (host, addr) = do
-        ereply <- ntpSend sock addr >> ntpRecv sock
-        case ereply of
-            Left err    -> putStrLn ("Error: " ++ err)
-            Right reply -> printCsv host addr reply
+    loop servers sock = do
+        servers' <- mapM (update sock) servers
+        --putStrLn "NTP Server Offsets"
+        putStrLn (intercalate "," (map showOffset (calcOffsets servers')))
+        --putStrLn ""
+        threadDelay 1000000
+        loop servers' sock
 
-printCsv :: HostName -> SockAddr -> NTPReply -> IO ()
-printCsv host addr reply = do
-    now <- getCurrentTime
-    index <- rdtsc
+    update sock svr = do
+        svr' <- updateServer sock svr
+        return svr'
 
-    let offset = localClockOffset reply
-        delay  = roundtripDelay reply
-        ntp    = offset `addUTCTime` now
-        time   = formatTime defaultTimeLocale "%T.%q" ntp
-        csv    = intercalate ","
-                 [ host
-                 --, show addr
-                 , showMilli delay
-                 , showMilli offset
-                 , show index
-                 , time ]
+    showAddress = (takeWhile (/= ':') . show) . svrAddress
 
-    putStrLn csv
+    showOffset (_, offset) = (maybe "Unknown" showMilli offset)
+
+    -- showOffset (Server {..}, offset) = printf "%-18s %-16s %s\n"
+    --     svrHostName
+    --     ((reverse . drop 4 . reverse . show) svrAddress)
+    --     (maybe "Unknown Offset" showMilli offset)
+
+------------------------------------------------------------------------
+-- Types
+
+data Server = Server {
+      svrHostName :: HostName
+    , svrAddress  :: SockAddr
+    , svrRecords  :: [Record]
+    } deriving (Show)
+
+type Record = (Timestamp, UTCTime)
 
 ------------------------------------------------------------------------
 
-ntpSocket :: IO Socket
-ntpSocket = socket AF_INET Datagram defaultProtocol
-
-ntpAddrs :: HostName -> IO [SockAddr]
-ntpAddrs host =
-    map addrAddress <$> getAddrInfo hints (Just host) (Just "ntp")
+-- | Resolves a list of IP addresses registered for the specified
+-- hostname and creates 'Server' instances for each of them.
+getServers :: HostName -> IO [Server]
+getServers host =
+    map (mkServer . addrAddress) <$> getHostAddrInfo
   where
+    mkServer addr   = Server host addr []
+    getHostAddrInfo = getAddrInfo hints (Just host) (Just "ntp")
+
     hints = Just defaultHints { addrFamily     = AF_INET
                               , addrSocketType = Datagram }
 
-ntpSend :: Socket -> SockAddr -> IO ()
-ntpSend sock addr = do
-    now <- getCurrentTime
-    let msg = emptyNTPMsg { ntpTransmitTime = now }
-        bs  = runPut (put msg)
-    sendAllTo sock bs addr
+updateServer :: Socket -> Server -> IO Server
+updateServer sock svr = do
+    t1 <- ntpSend sock (svrAddress svr)
+    rs <- toList =<< ntpRecv sock t1
+    return (insertRecords svr rs)
+  where
+    toList (Left err) = return [] --putStrLn ("Error: " ++ err) >> return []
+    toList (Right x)  = return [x]
 
-ntpRecv :: Socket -> IO (Either String NTPReply)
-ntpRecv sock = do
-    bs <- recv sock 128
-    now <- getCurrentTime
-    return (NTPReply now <$> runGet get bs)
+insertRecords :: Server -> [Record] -> Server
+insertRecords svr xs = svr { svrRecords = records }
+  where
+    records = take 5 (xs ++ svrRecords svr)
+
+calcOffsets :: [Server] -> [(Server, Maybe NominalDiffTime)]
+calcOffsets []     = []
+calcOffsets (x:xs) = (x, Just 0) : map go xs
+  where
+    (i, t) = (head . svrRecords) x
+    go x'  = let t'     = timeAt i (svrRecords x')
+                 offset = (`diffUTCTime` t) <$> t'
+             in (x', offset)
+
+timeAt :: Timestamp -> [Record] -> Maybe UTCTime
+timeAt i rs = case span ((> i) . fst) rs of
+    (r2:_, r1:_) -> Just (interp i r1 r2)
+    _            -> Nothing
+
+interp :: Timestamp -> Record -> Record -> UTCTime
+interp i (i0, t0) (i1, t1) = t
+  where
+    t = ((t1 `diffUTCTime` t0) * alpha) `addUTCTime` t0
+    alpha = fromIntegral (i - i0) / fromIntegral (i1 - i0)
 
 ------------------------------------------------------------------------
 
-data NTPReply = NTPReply UTCTime NTPMsg
-    deriving (Show)
+udpSocket :: IO Socket
+udpSocket = socket AF_INET Datagram defaultProtocol
 
-roundtripDelay :: NTPReply -> NominalDiffTime
-roundtripDelay = go . timestamps
+ntpSend :: Socket -> SockAddr -> IO Timestamp
+ntpSend sock addr = do
+    now <- getCurrentTime
+    timestamp <- getTimestamp
+    let msg = emptyNTPMsg { ntpTransmitTime = now }
+        bs  = runPut (put msg)
+    sendAllTo sock bs addr
+    return timestamp
+
+ntpRecv :: Socket -> Timestamp -> IO (Either String Record)
+ntpRecv sock t1 = do
+    mbs <- timeout 1000000 (recv sock 128)
+    t4 <- getTimestamp
+    return $ case mbs of
+      Nothing -> Left "Timed out"
+      Just bs -> record t4 <$> runGet get bs
   where
-    go (t1,t2,t3,t4) = (t4 `diffUTCTime` t1) - (t3 `diffUTCTime` t2)
+    record t4 NTPMsg {..} =
+        (mean t1 t4, meanUTC ntpReceiveTime ntpTransmitTime)
 
-localClockOffset :: NTPReply -> NominalDiffTime
-localClockOffset = go . timestamps
-  where
-    go (t1,t2,t3,t4) = ((t2 `diffUTCTime` t1) - (t4 `diffUTCTime` t3)) / 2
+------------------------------------------------------------------------
 
-timestamps :: NTPReply -> (UTCTime, UTCTime, UTCTime, UTCTime)
-timestamps (NTPReply ntpDestinationTime NTPMsg {..}) =
-    ( ntpOriginateTime   -- time request sent by client
-    , ntpReceiveTime     -- time request received by server
-    , ntpTransmitTime    -- time reply sent by server
-    , ntpDestinationTime -- time reply received by client
-    )
+mean :: Integral a => a -> a -> a
+mean x y = ((y - x) `div` 2) + x
+
+meanUTC :: UTCTime -> UTCTime -> UTCTime
+meanUTC x y = ((y `diffUTCTime` x) / 2) `addUTCTime` x
 
 showMilli :: NominalDiffTime -> String
-showMilli t = printf "%+.4fms" ms
+--showMilli t = printf "%+.4fms" ms
+showMilli t = printf "%.4f" ms
   where
     ms = (1000 :: Double) * (realToFrac t)
+
+------------------------------------------------------------------------
+
+type Timestamp = Word64
+
+getTimestamp :: IO Timestamp
+getTimestamp = rdtsc
+
+--getTimestamp :: IO Timestamp
+--getTimestamp = do
+--    TimeSpec {..} <- getTime Monotonic
+--    let timestamp = (fromIntegral sec) * 1000000
+--                  + ((fromIntegral nsec) `div` 1000)
+--    return timestamp
+
+--getTimestamp = do
+--    now <- getCurrentTime
+--    let diff = (now `diffUTCTime` origin)
+--        timestamp = truncate (10000000 * diff)
+--    return timestamp
+--  where
+--    origin = UTCTime (fromGregorian 2012 1 1) 0
