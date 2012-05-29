10e3edf6c412817f9cd98e6ec00933f81e6618f3
Jacob Stanley <jystic@jystic.com>
2012-05-30 03:34:27 +0800

    Also check c:\ntp for ntp installation

--- src/Win32Compat.hs
+++ src/Win32Compat.hs
@@ -4,14 +4,25 @@ module Win32Compat (getNTPConf) where
 
 #ifdef mingw32_HOST_OS
 
-import System.Win32 (sHGetFolderPath)
+import Control.Monad (filterM)
+import System.Directory (doesFileExist)
+import System.FilePath (takeDrive, normalise, (</>))
 import System.Win32 (nullHANDLE, cSIDL_PROGRAM_FILES, sHGFP_TYPE_CURRENT)
-import System.FilePath (normalise, (</>))
+import System.Win32 (sHGetFolderPath)
 
 getNTPConf :: IO FilePath
 getNTPConf = do
     pf <- getProgramFiles
-    return (normalise (pf </> "ntp/etc/ntp.conf"))
+
+    let conf     = "ntp/etc/ntp.conf"
+        pfConf   = normalise (pf </> conf)
+        rootConf = normalise (takeDrive pf </> conf)
+
+    paths <- filterM doesFileExist [pfConf, rootConf]
+
+    case paths of
+        (path:_) -> return path
+        []       -> error "getNTPConf: could not find ntp installation"
 
 getProgramFiles :: IO String
 getProgramFiles = sHGetFolderPath
@@ -20,6 +31,10 @@ getProgramFiles = sHGetFolderPath
 #else
 
 getNTPConf :: IO FilePath
-getNTPConf = return "/etc/ntp.conf"
+getNTPConf = do
+    let conf = "/etc/ntp.conf"
+    ok <- doesFileExist conf
+    if ok then return conf
+          else error "getNTPConf: could not find ntp config file"
 
 #endif
