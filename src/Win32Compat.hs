8dd3f7c824de7b2ad38946b3582c8f4acb59be08
Jacob Stanley <jystic@jystic.com>
2012-06-12 01:54:25 +0800

    Config finding code now uses registry to find NTP service

--- src/Win32Compat.hs
+++ src/Win32Compat.hs
@@ -1,40 +1,56 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Win32Compat (getNTPConfPath) where
 
+import System.Directory (doesFileExist)
+
 #ifdef mingw32_HOST_OS
 
-import Control.Monad (filterM)
-import System.Directory (doesFileExist)
-import System.FilePath (takeDrive, normalise, (</>))
-import System.Win32 (nullHANDLE, cSIDL_PROGRAM_FILES, sHGFP_TYPE_CURRENT)
-import System.Win32 (sHGetFolderPath)
+import Control.Exception (IOException, handle, bracket)
+import Data.List (find, tails, isPrefixOf)
+import System.Win32 (hKEY_LOCAL_MACHINE, regOpenKey, regCloseKey, regQueryValue)
+
+#endif
+
+------------------------------------------------------------------------
 
 getNTPConfPath :: IO FilePath
 getNTPConfPath = do
-    pf <- getProgramFiles
+    conf <- findConfig
+    ok   <- doesFileExist conf
+    if ok then return conf
+          else error $ "getNTPConfPath: " ++ conf ++ " does not exist"
 
-    let conf     = "ntp/etc/ntp.conf"
-        pfConf   = normalise (pf </> conf)
-        rootConf = normalise (takeDrive pf </> conf)
+#ifdef mingw32_HOST_OS
 
-    paths <- filterM doesFileExist [pfConf, rootConf]
+findConfig :: IO FilePath
+findConfig = do
+    cmd <- handleIOError getServiceCmd
+    case findConf cmd of
+      Just conf -> return conf
+      Nothing   -> error $ "findConfig: could not find 'ntp.conf' location\n"
+                        ++ "NTP service command: " ++ cmd
+  where
+    getServiceCmd = bracket (regOpenKey hive key) regCloseKey
+                            (flip regQueryValue value)
 
-    case paths of
-        (path:_) -> return path
-        []       -> error "getNTPConfPath: could not find ntp installation"
+    hive  = hKEY_LOCAL_MACHINE
+    key   = "SYSTEM\\CurrentControlSet\\Services\\ntp"
+    value = Just "ImagePath"
 
-getProgramFiles :: IO String
-getProgramFiles = sHGetFolderPath
-    nullHANDLE cSIDL_PROGRAM_FILES nullHANDLE sHGFP_TYPE_CURRENT
+    findConf :: String -> Maybe String
+    findConf = fmap (takePath . drop 3) . find ("-c " `isPrefixOf`) . tails
+
+    takePath ('"':xs) = takeWhile (/= '"') xs
+    takePath xs       = takeWhile (/= ' ') xs
+
+    handleIOError = handle (\(_ :: IOException) ->
+        error "findConfig: NTP service is not installed on this computer")
 
 #else
 
-getNTPConfPath :: IO FilePath
-getNTPConfPath = do
-    let conf = "/etc/ntp.conf"
-    ok <- doesFileExist conf
-    if ok then return conf
-          else error "getNTPConfPath: could not find ntp config file"
+findConfig :: IO FilePath
+findConfig = return "/etc/ntp.conf"
 
 #endif
