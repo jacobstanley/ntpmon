96864e4ddcab8b871b38ca34174b0bc6baf914b5
Jacob Stanley <j.stanley@fugro.com.au>
2012-06-14 15:15:27 +0800

    Fixed bug where registry query only supported REG_SZ
    - REG_MULTI_SZ and REG_EXPAND_SZ are also possible string types

--- src/Network/NTP/ConfigFinder.hs
+++ src/Network/NTP/ConfigFinder.hs
@@ -8,8 +8,14 @@ import System.Directory (doesFileExist)
 #ifdef mingw32_HOST_OS
 
 import Control.Exception (IOException, handle, bracket)
+import Control.Monad (when)
 import Data.List (find, tails, isPrefixOf)
-import System.Win32 (hKEY_LOCAL_MACHINE, regOpenKey, regCloseKey, regQueryValue)
+import Foreign (withForeignPtr, nullPtr, castPtr, peek)
+import Foreign.Marshal (alloca, allocaArray0, maybeWith)
+import System.Win32 (HKEY, hKEY_LOCAL_MACHINE)
+import System.Win32 (rEG_SZ, rEG_MULTI_SZ, rEG_EXPAND_SZ)
+import System.Win32 (regOpenKey, regCloseKey, c_RegQueryValueEx)
+import System.Win32 (withTString, peekTString, failUnlessSuccess)
 
 #endif
 
@@ -33,7 +39,7 @@ findPossibleConfig = do
                         ++ "NTP service command: " ++ cmd
   where
     getServiceCmd = bracket (regOpenKey hive key) regCloseKey
-                            (flip regQueryValue value)
+                            (flip regQueryValueString value)
 
     hive  = hKEY_LOCAL_MACHINE
     key   = "SYSTEM\\CurrentControlSet\\Services\\ntp"
@@ -45,8 +51,30 @@ findPossibleConfig = do
     takePath ('"':xs) = takeWhile (/= '"') xs
     takePath xs       = takeWhile (/= ' ') xs
 
-    handleIOError = handle (\(_ :: IOException) ->
-        error "findConfig: NTP service is not installed on this computer")
+    handleIOError = handle $ \(e :: IOException) ->
+        error $ "findConfig: could not read NTP service details\n" ++ show e
+
+-- TODO [ Submit bug report to Win32 package ]
+-- TODO 'System.Win32.Registry.regQueryValue' throws if the key type is
+-- TODO not REG_SZ, even though REG_MULTI_SZ and REG_EXPAND_SZ are also
+-- TODO valid string types. This function fixes that problem.
+regQueryValueString :: HKEY -> Maybe String -> IO String
+regQueryValueString key mb_subkey =
+    withForeignPtr key $ \ p_key ->
+    maybeWith withTString mb_subkey $ \ c_subkey ->
+    alloca $ \ p_ty ->
+    alloca $ \ p_value_len -> do
+    failUnlessSuccess "RegQueryValue" $
+        c_RegQueryValueEx p_key c_subkey nullPtr p_ty nullPtr p_value_len
+    ty <- peek p_ty
+    when (ty `notElem` [rEG_SZ, rEG_MULTI_SZ, rEG_EXPAND_SZ]) $
+        ioError $ userError $ "RegQueryValue: expected value to be a string "
+                           ++ "(REG_SZ, REG_MULTI_SZ or REG_EXPAND_SZ)"
+    value_len <- peek p_value_len
+    allocaArray0 (fromIntegral value_len) $ \ c_value -> do
+        failUnlessSuccess "RegQueryValue" $
+            c_RegQueryValueEx p_key c_subkey nullPtr p_ty c_value p_value_len
+        peekTString (castPtr c_value)
 
 #else
 
